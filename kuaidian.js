2b776590573b3c77bbf7ce15ecbf7eb86c97dd93
Jiyao <jiyaosuny@163.com>
2024-01-22 01:17:15 +0800

    11

--- kuaidian.js
+++ kuaidian.js
@@ -1,1 +1,24 @@
-console.log ('first version');
+// ==UserScript==
+// @name         快点众包界面优化
+// @namespace    kdzb.cc
+// @version      1.0
+// @description  快点众包界面优化
+// @author       Jiyao
+// @match        https://kdzb.cc/*
+// @icon         https://kdzb.cc/logo.png
+// @license      AGPL-3.0
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 去除watermar
+    GM_addStyle('.watermark{visibility: hidden !important;}');
+    GM_addStyle('.income-card{background: black !important;border-style: dashed;}');
+    GM_addStyle('.level-info{background: black !important;border-style: dashed;}');
+    GM_addStyle('.ranking{background: black !important;border-style: dashed;}');
+    GM_addStyle('.personal .num-box .num-item{border-style: dashed;}');
+    GM_addStyle('.goods-camp{background: black !important;}');
+    GM_addStyle('.el-badge__content.is-fixed{font-size: xx-large !important;}');
+ })();
