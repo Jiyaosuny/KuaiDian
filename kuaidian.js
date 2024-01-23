3211974cd0ea25ac3296d3e2e35f4b99ae471192
Jiyao <jiyaosuny@163.com>
2024-01-23 09:39:10 +0800

    ceshi

--- kuaidian.js
+++ kuaidian.js
@@ -15,10 +15,16 @@
 
     // 去除watermar
     GM_addStyle('.watermark{visibility: hidden !important;}');
-    GM_addStyle('.income-card{background: black !important;border-style: dashed;}');
+    //今日收入
+    GM_addStyle('.income-card{background: black !important;border-style: dashed;}；.page .level-info .title{color:#ffffff}');
+    //等级展示
     GM_addStyle('.level-info{background: black !important;border-style: dashed;}');
+    //今日排名
     GM_addStyle('.ranking{background: black !important;border-style: dashed;}');
+    //个人概况
     GM_addStyle('.personal .num-box .num-item{border-style: dashed;}');
+
     GM_addStyle('.goods-camp{background: black !important;}');
+
     GM_addStyle('.el-badge__content.is-fixed{font-size: xx-large !important;}');
  })();
